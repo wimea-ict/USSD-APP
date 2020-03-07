b96a31bdfaad0a075629710d38cb0f3d2221aa30
Amoko Ivan <amokoivan@gmail.com>
2020-03-07 14:04:24 -0600

    Adding the latest USSD code for WIDS

---
+++ Dd.php
@@ -0,0 +1,32 @@
+<?php 
+ob_start();
+include_once 'ussdlog.php';
+include_once 'connectionStr.php';
+echo "eor";
+
+// $conn = $this->connectionStr->ConnectionFc();
+// $w = mysql_error($conn);
+// var_dump($w);
+// echo "dsds".$conn;
+$dbname = '';
+$dbuser = '';
+$dbpass = '';
+//$dbpass = '';
+$dbhost = 'localhost';
+$conn = new mysqli($dbhost, $dbuser,$dbpass,$dbname);
+$sql = "SELECT Level8 FROM ussdtransaction WHERE Msisdn = 256701586693 ORDER BY TranId DESC LIMIT 1 ";
+            $query = $conn->query($sql);
+            if($query->num_rows > 0){
+              $field= $query->fetch_assoc();
+              $language = $field['Level8'];
+              echo "this is ".$language;
+            }
+            else{
+                echo "this is ";
+              //logFile("No such record for ".$callerNumber);
+            }
+
+
+
+
+        ?>
